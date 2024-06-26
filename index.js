498df56eb7c0f8843e80db7b1dda88b9a2e91b52
ruben hernan pared <hernan2014@hotmail.com>
2024-06-26 10:18:39 -0300

    peticiones

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.get('/', (req, res) => {
         <h1>Bienvenido a mi aplicación Express!</h1>
         <p><a href="/about">Acerca de</a></p>
         <p><a href="/contact">Contacto</a></p>
+        <p><a href="/saludo/Visitante">Saludar a Visitante</a></p>
     `);
 });
 
@@ -16,19 +17,32 @@ app.get('/about', (req, res) => {
         <h1>Esta es una página sobre la aplicación Express.</h1>
         <p><a href="/">Inicio</a></p>
         <p><a href="/contact">Contacto</a></p>
+        <p><a href="/saludo/Visitante">Saludar a Visitante</a></p>
     `);
 });
 
 // Ruta '/contact' que devuelve detalles de contacto
 app.get('/contact', (req, res) => {
     res.send(`
-        <h1>Contacta con nosotros en: hernan2014@hotmail.com</h1>
+        <h1>Contacta con nosotros en: contacto@example.com</h1>
         <p><a href="/">Inicio</a></p>
         <p><a href="/about">Acerca de</a></p>
+        <p><a href="/saludo/Visitante">Saludar a Visitante</a></p>
     `);
 });
 
-// El servidor escucha en el puerto 3000
+// Ruta dinámica que varía según el parámetro recibido
+app.get('/saludo/:nombre', (req, res) => {
+    const nombre = req.params.nombre;
+    res.send(`
+        <h1>¡Hola, ${nombre}!</h1>
+        <p><a href="/">Inicio</a></p>
+        <p><a href="/about">Acerca de</a></p>
+        <p><a href="/contact">Contacto</a></p>
+    `);
+});
+
+// Configura el servidor para escuchar en el puerto 3000
 app.listen(3000, () => {
-    console.log('Aplicación ejecutándose en el puerto 3000');
+    console.log('Servidor corriendo en http://localhost:3000');
 });
